022c1ec3b98827fe98e532439fa3e99338505841
Moneta <moneta.lukasz@gmail.com>
2023-01-14 13:52:49 +0100

    first commit

---
+++ src/main/java/com/baal/WebShop/WebShopApplication.java
@@ -0,0 +1,13 @@
+package com.baal.WebShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baal/WebShop/WebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baal.WebShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
