701e15b3ab0a9b24c96568a8a510518727553cbc
Piotr Mackowiak <mackowiak55@gmail.com>
2023-01-19 21:07:05 +0100

    feature: Add findCategoryById method in CategoryController

--- src/main/java/com/baal/WebShop/controller/CategoryController.java
+++ src/main/java/com/baal/WebShop/controller/CategoryController.java
@@ -4,10 +4,14 @@ import com.baal.WebShop.DTO.CategoryDTO;
 import com.baal.WebShop.DTO.CreateCategoryDTO;
 import com.baal.WebShop.service.CategoryService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +29,11 @@ public class CategoryController {
             @Valid @RequestBody CreateCategoryDTO createCategoryDTO) {
         CategoryDTO categoryDTO = categoryService.createCategory(createCategoryDTO);
         return new ResponseEntity<>(categoryDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDTO> findCategoryById(@Min(1) @NotNull @PathVariable Long id) {
+        CategoryDTO categoryDTO = categoryService.findById(id);
+        return new ResponseEntity<>(categoryDTO, HttpStatus.OK);
     }
 }
