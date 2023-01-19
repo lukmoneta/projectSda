abb906f97cda036a16c2f10c4e42cd2acff2a136
Marek <wieckowskimarek1@gmail.com>
2023-01-19 23:01:12 +0100

    feature: add AddressRepository

---
+++ src/main/java/com/baal/WebShop/repository/AddressRepository.java
@@ -0,0 +1,7 @@
+package com.baal.WebShop.repository;
+
+import com.baal.WebShop.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
