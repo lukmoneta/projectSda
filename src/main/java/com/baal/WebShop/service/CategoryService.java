8b4f6d41cad0f5236fd7015f5a26e9a03a002806
Piotr Mackowiak <mackowiak55@gmail.com>
2023-01-19 20:52:50 +0100

    feature: Add findById method to CategoryServiceImpl

--- src/main/java/com/baal/WebShop/service/CategoryService.java
+++ src/main/java/com/baal/WebShop/service/CategoryService.java
@@ -3,10 +3,13 @@ package com.baal.WebShop.service;
 import com.baal.WebShop.DTO.CategoryDTO;
 import com.baal.WebShop.DTO.CreateCategoryDTO;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 public interface CategoryService {
 
 CategoryDTO createCategory(@Valid CreateCategoryDTO createCategoryDTO);
+CategoryDTO findById(@NotNull @Min(1) Long id);
 }
