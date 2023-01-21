2bc0ba444df40122190663a3ed27486f9e3cd50a
Piotr Mackowiak <mackowiak55@gmail.com>
2023-01-21 22:13:27 +0100

    feature: Add admin bootstrap template

--- src/main/java/com/baal/WebShop/controller/tymyleaf/HomeController.java
+++ src/main/java/com/baal/WebShop/controller/tymyleaf/HomeController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/admin")
     public String getHome() {
-        return "index";
+        return "admin/index";
     }
 }
