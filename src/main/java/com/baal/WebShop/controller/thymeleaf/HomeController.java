faf21974fa793a6e3a7f2f3174dc5cb83be76053
Piotr Mackowiak <mackowiak55@gmail.com>
2023-01-22 09:00:09 +0100

    feature: Add findAllProducts

--- src/main/java/com/baal/WebShop/controller/thymeleaf/HomeController.java
+++ src/main/java/com/baal/WebShop/controller/thymeleaf/HomeController.java
@@ -1,4 +1,4 @@
-package com.baal.WebShop.controller.tymyleaf;
+package com.baal.WebShop.controller.thymeleaf;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
