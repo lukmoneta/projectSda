9c25b628e05168fab2687c910975a3a207dbfce9
Marek <wieckowskimarek1@gmail.com>
2023-01-22 11:21:53 +0100

    feature: add method getUsers

--- src/main/java/com/baal/WebShop/service/UserService.java
+++ src/main/java/com/baal/WebShop/service/UserService.java
@@ -5,7 +5,11 @@ import com.baal.WebShop.DTO.UserDTO;
 import jakarta.validation.Valid;
 import org.springframework.validation.annotation.Validated;
 
+import java.util.List;
+
 @Validated
 public interface UserService {
     UserDTO createUser(@Valid CreateUserDTO createUserDTO);
+
+    List<UserDTO> getUsers();
 }
