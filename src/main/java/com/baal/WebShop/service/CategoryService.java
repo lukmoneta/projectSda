39f5c342785563d45c3c61b508511f03787f6581
Marek <wieckowskimarek1@gmail.com>
2023-01-22 11:15:45 +0100

    feature: add method getCategories

--- src/main/java/com/baal/WebShop/service/CategoryService.java
+++ src/main/java/com/baal/WebShop/service/CategoryService.java
@@ -7,9 +7,13 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.validation.annotation.Validated;
 
+import java.util.List;
+
 @Validated
 public interface CategoryService {
 
 CategoryDTO createCategory(@Valid CreateCategoryDTO createCategoryDTO);
 CategoryDTO findById(@NotNull @Min(1) Long id);
+
+List<CategoryDTO> getCategories();
 }
