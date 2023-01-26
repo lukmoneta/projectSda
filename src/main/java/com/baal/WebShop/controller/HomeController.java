e52f7a5fba9f61cfb4617ae694401014b4538d9b
Piotr Mackowiak <mackowiak55@gmail.com>
2023-01-26 20:37:33 +0100

    feature: Add product list display on main page

--- src/main/java/com/baal/WebShop/controller/HomeController.java
+++ src/main/java/com/baal/WebShop/controller/HomeController.java
@@ -1,18 +1,27 @@
 package com.baal.WebShop.controller;
 
+import com.baal.WebShop.DTO.ProductDTO;
+import com.baal.WebShop.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+private final ProductService productService;
+    @GetMapping("/")
+    public String getHome(Model model){
 
-    @GetMapping("")
-    public String getHome(){
+        List<ProductDTO> productList = productService.getProducts();
+        model.addAttribute("products", productList);
 
-        return "/index";
+        return "index";
     }
+
 
 
 }
--- src/main/java/com/baal/WebShop/controller/admin/UserOrderController.java
+++ src/main/java/com/baal/WebShop/controller/admin/UserOrderController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,9 +20,10 @@ public class UserOrderController {
     private final UserOrderService userOrderService;
 
     @GetMapping("")
-    public ResponseEntity<List<UserOrderDTO>> getCategories() {
+    public String getCategories(Model model) {
         List<UserOrderDTO> userOrders = userOrderService.getUserOrders();
-        return new ResponseEntity<>(userOrders, HttpStatus.OK);
+        model.addAttribute("userOrders",userOrders);
+        return "/admin/orderList";
     }
 
 
