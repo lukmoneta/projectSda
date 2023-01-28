c744fdfd6757d47bbb8228b95cd2f85a51451c08
Piotr Mackowiak <mackowiak55@gmail.com>
2023-01-28 11:30:10 +0100

    feature: Add addCategory.html feature: Add create category feature

--- src/main/java/com/baal/WebShop/controller/admin/CategoryController.java
+++ src/main/java/com/baal/WebShop/controller/admin/CategoryController.java
@@ -1,11 +1,16 @@
 package com.baal.WebShop.controller.admin;
 
 import com.baal.WebShop.DTO.CategoryDTO;
+import com.baal.WebShop.DTO.CreateCategoryDTO;
+import com.baal.WebShop.DTO.CreateProductDTO;
+import com.baal.WebShop.DTO.ProductDTO;
 import com.baal.WebShop.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,7 +27,18 @@ public class CategoryController {
         model.addAttribute("categories",categories);
         return "admin/categoryList";
     }
+    @GetMapping("/add")
+    public String addCategory(Model model) {
+        model.addAttribute("createCategory", new CreateCategoryDTO(null));
+        model.addAttribute("categories", categoryService.getCategories());
+        return "admin/addCategory";
+    }
 
+    @PostMapping(value = "/add")
+    public String addNewCategory(@ModelAttribute CreateCategoryDTO createCategoryDTO) {
+        categoryService.createCategory(createCategoryDTO);
+        return "redirect:/admin/category";
+    }
 
 
 }
