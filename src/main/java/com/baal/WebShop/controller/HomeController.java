43a7eaf3aed5db1ab4cf73cd466fd96138cf1017
Piotr Mackowiak <mackowiak55@gmail.com>
2023-01-28 23:15:10 +0100

    feature: add some upgrades

--- src/main/java/com/baal/WebShop/controller/HomeController.java
+++ src/main/java/com/baal/WebShop/controller/HomeController.java
@@ -1,14 +1,20 @@
 package com.baal.WebShop.controller;
 
 import com.baal.WebShop.DTO.CreateOrderLineDTO;
+import com.baal.WebShop.DTO.GivenUpdateOrdeLineDTO;
 import com.baal.WebShop.DTO.OrderLineWithProductNameDTO;
 import com.baal.WebShop.DTO.ProductDTO;
+import com.baal.WebShop.DTO.UpdateOrderlineDTO;
+import com.baal.WebShop.service.OrderLineService;
+import com.baal.WebShop.service.ProductService;
 import com.baal.WebShop.repository.service.OrderLineService;
 import com.baal.WebShop.repository.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -40,9 +46,23 @@ public class HomeController {
     public String getCart(Model model) {
         long numberOfOrderLines = orderLineService.numberOfOrderLines();
         List<OrderLineWithProductNameDTO> orderLinesWithProductNames = orderLineService.getOrderLinesWithProductNames();
+        model.addAttribute("updateDTO",new GivenUpdateOrdeLineDTO(null,null,null,null));
         model.addAttribute("numberOfOrderLines", numberOfOrderLines);
         model.addAttribute("orderLines", orderLinesWithProductNames);
         return "cart";
+    }
+
+    @PostMapping("/cart")
+    public String updateOrderLine(@ModelAttribute("orderLines") List<OrderLineWithProductNameDTO> orderLineWithProductNameDTOS) {
+
+
+        return "redirect:/cart";
+    }
+
+    @PostMapping("/addQuantity")
+    public String updateQuantity(@ModelAttribute UpdateOrderlineDTO updateOrderlineDTO) {
+        orderLineService.updateOrderQuantity(updateOrderlineDTO);
+        return "redirect:/cart";
     }
 
 
